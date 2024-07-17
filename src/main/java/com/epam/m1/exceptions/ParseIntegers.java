dac43215b7d132f4e77e7fcbcada04c166d90222
Axrich <116589250+Axrich@users.noreply.github.com>
2024-07-17 14:48:05 +0500

    Update ParseIntegers.java

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -1,10 +1,7 @@
 package com.epam.m1.exceptions;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
-import java.lang.*;
 import java.util.stream.Collectors;
 
 /**
@@ -14,33 +11,27 @@ import java.util.stream.Collectors;
 public class ParseIntegers {
 
     private static final List<String> WORDS = Arrays.asList(
-                    "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
-                            .split(" "));
+            "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
+                    .split(" "));
 
     public static void main(String[] args) {
-        Iterator<String> words = WORDS.iterator();
         int sum = 0;
-        String justWords = "";
-        while (words.hasNext()) {
-            String next = words.next();
-            try {
-                int number = Integer.parseInt(next);
-            } catch (NumberFormatException e) {
-                e.getMessage();
-            }
-        }
+        StringBuilder justWords = new StringBuilder();
 
         for (String word : WORDS) {
-            if (word.matches("\\d+")) {
-                sum += Integer.parseInt(word);
+            try {
+                int number = Integer.parseInt(word);
+                sum += number;
+            } catch (NumberFormatException e) {
+                // Append non-numeric words to justWords with a space delimiter
+                justWords.append(word).append(" ");
             }
         }
 
-        String forJustWords = " JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations.";
-
-        justWords = forJustWords.replaceAll("\\d+ ", "");
+        // Trim any trailing space from justWords
+        String justWordsString = justWords.toString().trim();
 
         System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords);
+        System.out.println("Just words: " + justWordsString);
     }
 }
